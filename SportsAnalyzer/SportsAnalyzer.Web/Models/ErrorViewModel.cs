b885031bec5e23ffada302866e81aae81b37b5dc
shopoff <shopov91@gmail.com>
2018-02-21 21:52:16 +0200

    web proj added

---
+++ SportsAnalyzer/SportsAnalyzer.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SportsAnalyzer.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
